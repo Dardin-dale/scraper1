668bef0426ef349c559f2d2e9da6d6debf622289
Dardin <logandalec@gmail.com>
2018-12-22 21:48:09 -0800

    server fix

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
-
-app.use(express.urlencoded({extended:true}));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 var exphbs = require("express-handlebars");
